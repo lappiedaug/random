12c9f88fa022eb9277186bd40114f2c863e79e92
lappiedaug <90304685+lappiedaug@users.noreply.github.com>
2021-09-11 22:21:07 +0300

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,38 @@
+from time import sleep
+
+tuples = [0]
+
+max_val = 26
+max_list = 3
+
+pasws = 'malas'
+
+while True:
+	
+
+	i = 0
+	criteria = True
+	while i<len(tuples):
+		
+		if criteria:
+			tuples[i]+=1
+			tuples[i]%=max_val
+		elif not criteria:
+			break
+		
+		
+		if all(list(map(lambda x:x==0,tuples))) and i==(len(tuples)-1):
+			tuples.append(0)
+			
+		criteria = (tuples[i]==0)
+		i+=1
+		
+		Try = ''
+		for m in tuples:
+			Try += chr(97+m)
+		
+		print(Try,end='\r')
+
+	if Try==pasws:
+		break
+		
